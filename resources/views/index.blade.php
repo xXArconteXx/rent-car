61a59e5e7636e4fca72a00f955c2f9888446ded5
xXArconteXx <agustinrallysport@gmail.com>
2022-02-10 21:11:56 +0000

    comment

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -56,10 +56,10 @@
     </div>
 
     <section>
-        <img src="{{URL::asset('image/monovolumen.webp')}}">
+        <!-- <img src="{{URL::asset('image/monovolumen.webp')}}">
         <img src="{{URL::asset('image/furgoneta.webp')}}">
         <img src="{{URL::asset('image/electrico.webp')}}">
-        <img src="{{URL::asset('image/camion.webp')}}">
+        <img src="{{URL::asset('image/camion.webp')}}"> -->
     </section>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
